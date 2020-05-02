cc6a83370864969b1af98153600a381bbbe20b8a
aladdin99 <2025033966@qq.com>
2020-05-02 17:04:02 +0800

    update

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: './starTrail/dist/',
     proxyTable: {},
 
     // Various Dev Server settings
